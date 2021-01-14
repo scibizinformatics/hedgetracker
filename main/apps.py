b5a948eebd2e740ad5cd1189db1993774083ae03
jethro <jgalbano1@up.edu.ph>
2021-01-14 14:45:06 +0800

    Separated process for transaction tracker and web server

--- main/apps.py
+++ main/apps.py
@@ -1,4 +1,3 @@
-from django.db.models.signals import post_migrate
 from django.apps import AppConfig
 
 
@@ -7,9 +6,3 @@ class MainConfig(AppConfig):
 
     def ready(self):
         import main.signals
-        post_migrate.connect(self.run_txn_tracker, sender=self)
-
-
-    def run_txn_tracker(self, *args, **kwargs):
-        from main.bchd import transactions_tracker
-        transactions_tracker.run()
