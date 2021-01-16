2d88163cef55165ce636ff6e10ddce73ff1284d6
jethro <jgalbano1@up.edu.ph>
2021-01-16 14:54:01 +0800

    Added fabfile for server syncing

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+from fabric.contrib.project import rsync_project
+from fabric.api import env, run, local
+
+
+env.hosts = ['ec2-34-220-75-30.us-west-2.compute.amazonaws.com']
+env.user = 'ubuntu'
+env.forward_agent = True
+env.key_filename = '/home/jethro/Documents/dev/scibiz-aws-keypair1.pem'
+
+def uname():
+    run('uname -a')
+
+def sync():
+    exc = [
+            'venv', '*.pyc', '.DS_Store', '*.pid',
+            '*~', '.git', '.gitignore', '*.log',
+            '*.rdb', '*sqlite*', '__pycache__', 'static',
+            'node_modules', '.env'
+        ]
+    rsync_project(
+        '/home/ubuntu',
+        delete=False,
+        exclude=exc, 
+        ssh_opts='-o stricthostkeychecking=no'
+    )
