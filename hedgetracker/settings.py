8150857e75345fabd517aad0f44632979618b006
jethro <jgalbano1@up.edu.ph>
2021-01-17 12:16:52 +0800

    Removed pagination in metrics for graph usage and retained settlements pagination by 10

--- hedgetracker/settings.py
+++ hedgetracker/settings.py
@@ -202,8 +202,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication'
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
 
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
